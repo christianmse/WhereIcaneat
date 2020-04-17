46b147fadd3934847c406bb357e9547d9c676556
unknown <christianmse@gmail.com>
2020-04-17 12:55:35 +0200

    issue #11 done

--- app/src/main/java/com/whereicaneat/ui/landing/RecyclerViewClickListener.kt
+++ app/src/main/java/com/whereicaneat/ui/landing/RecyclerViewClickListener.kt
@@ -6,5 +6,6 @@ import com.whereicaneat.domain.data.db.entities.Restaurante
 interface RecyclerViewClickListener {
     fun onRecyclerViewCartaClick(view: View, restaurante: Restaurante)
     fun onItemClick(it: View?, restaurante: Restaurante, position: Int)
+    fun onRecyclerViewSearchClick(nombreRestaurante: String)
 
 }
